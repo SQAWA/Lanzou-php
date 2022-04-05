b3f3823a4e554e4e94790d27d667590c510230e2
Yuuz12 <66946066+SQAWA@users.noreply.github.com>
2022-04-05 22:11:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,120 +1,171 @@
 <?php
-/**
- * @package Lanzou
- * @author Yuuz12
- * @version 1.2.1
- * @link https://yuuz12.top
- */
-include "../count.php";
-include "function.php";
+
 header('Access-Control-Allow-Origin:*');
 header('Content-type: application/json');
 error_reporting(0);
-
-$url = $_GET['url'];
-$pwd = $_GET['pwd'];
-$down = $_GET['down'];
-$error = curl($url);
-
-if (strpos($error,'文件取消分享了') !== false) {
-    $Json = array(
-    "code" => 201, 
-    "msg" => '文件已被取消分享了噢！',
-);
-$Json = json_encode($Json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-echo stripslashes($Json);
-return $Json;
-}elseif ($url != null) {
-    if ($pwd == NULL) {
-        $b = 'com/';
-        $c = '/';
-        $id = GetBetween($url, $b, $c);
-        $d = 'https://www.lanzoui.com/tp/' . $id;
-        $lanzouo = curl($d);
-        echo $lanzouo;
-        
-        if (strpos($lanzouo,'输入密码') !== false) {
-            $Json = array(
-            "code" => 202, 
-            "msg" => '输入访问密码再试试吧！',
-            );
-        $Json = json_encode($Json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        echo stripslashes($Json);
-        return $Json;
-        } else {
-        preg_match_all('/<div class=\"md\">(.*?)<span class=\"mtt\">/', $lanzouo, $name);
-        preg_match_all('/时间:<\\/span>(.*?)<span class=\\"mt2\\">/', $lanzouo, $time);
-        preg_match_all('/发布者:<\\/span>(.*?)<span class=\\"mt2\\">/', $lanzouo, $author);
-        preg_match('/submit.href = \'(.*?)\'/', $lanzou, $down1);
-        preg_match('/loaddown = \'(.*?)\';/', $lanzou, $down2);
-        preg_match('/domianload = \'(.*?)\';/', $lanzou, $down3);
-        preg_match_all('/<div class=\\"md\\">(.*?)<span class=\\"mtt\\">\\((.*?)\\)<\\/span><\\/div>/', $lanzouo, $size);
-            if (!empty($down2)){ // loaddown是否为空
-                $download = getRedirect($down1[1] . $down2[1]);
-            }
-            else{ // 如果为空就选择第二个
-                $download = getRedirect($down1[1] . $down3[1]);
-            }
-            if ($down == true) {
-                header("Location:" . $download);
-            } else {
-                $Json = array(
-                "code" => 200, 
-                "data" => array(
-                    "name" => $name[1][0], 
-                    "author" => $author[1][0], 
-                    "time" => $time[1][0], 
-                    "size" => $size[2][0], 
-                    "url" => $download,
-                    )
-            );
-            $Json = json_encode($Json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-            echo stripslashes($Json);
-            return $Json;
-            }
-        }
-    }
+$url = @$_REQUEST['url'];
+$pwd = @$_REQUEST['pwd'];
+$type = @$_REQUEST['type'];
+if (!empty($url)) {
+    str_replace('lanzous','lanzoux',$url);
     $b = 'com/';
     $c = '/';
     $id = GetBetween($url, $b, $c);
     $d = 'https://www.lanzoui.com/tp/' . $id;
-    $lanzouo = curl($d);
-    preg_match_all('/<div class=\"md\">(.*?)<span class=\"mtt\">/', $lanzouo, $name);
-    preg_match_all('/时间:<\\/span>(.*?)<span class=\\"mt2\\">/', $lanzouo, $time);
-    preg_match_all('/发布者:<\\/span>(.*?)<span class=\\"mt2\\">/', $lanzouo, $author);
-    preg_match_all('/<div class=\\"md\\">(.*?)<span class=\\"mtt\\">\\((.*?)\\)<\\/span><\\/div>/', $lanzouo, $size);
-    preg_match_all('/sign\':\'(.*?)\'/', $lanzouo, $sign);
-    $post_data = array('action' => 'downprocess', 'sign' => $sign[1][0], 'p' => $pwd);
-    $pwdurl = send_post('https://wwa.lanzoui.com/ajaxm.php', $post_data);
-    if(strpos($pwdurl,'"zt":0') !== false) {
+    $lanzou = curl($d);
+    if (empty($lanzou)){
+        $d = 'https://www.lanzoux.com/tp/' . $id;
+        $lanzou = curl($d);
+    }
+    if (strpos($lanzou,'文件取消分享了') || empty($lanzou) || strpos($lanzou,'访问地址错误')) {
         $Json = array(
-        "code" => 202, 
-        "msg" => '访问密码错辣！',
+            "code" => 404, 
+            "msg" => '文件取消分享了',
         );
-    $Json = json_encode($Json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-    echo stripslashes($Json);
-    return $Json;
-    } else {
-        $obj = json_decode($pwdurl, true);
-        $download = getRedirect($obj['dom'] . '/file/' . $obj['url']);
-        $Json = array(
-            "code" => 200, 
-            "data" => array(
-                "name" => $name[1][0], 
-                "author" => $author[1][0], 
-                "time" => $time[1][0], 
-                "size" => $size[2][0], 
-                "url" => $download
-            )
-        );
-        if ($down == true) {
-            header("Location:" . $download);
-        } else {
-            $Json = json_encode($Json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-            echo stripslashes($Json);
-            return $Json;
+    }
+    else{
+        if (strpos($lanzou,'输入密码') && empty($pwd)) {
+			$Json = array('code' => 202,'msg' => '请输入密码再试试吧');
+		}
+        else{
+            preg_match("/<div class=\"md\">(.*?)<span class=\"mtt\">/", $lanzou, $name);
+            preg_match('/时间:<\\/span>(.*?)<span class=\\"mt2\\">/', $lanzou, $time);
+            preg_match('/发布者:<\\/span>(.*?)<span class=\\"mt2\\">/', $lanzou, $author);
+            preg_match('/var domianload = \'(.*?)\'/', $lanzou, $domain);
+            if(empty($domain[1])){
+                preg_match('/submit.href = \'(.*?)\'/', $lanzou, $domain);
+            }
+            preg_match('/domianload \+ \'(.*?)\'/', $lanzou, $down2);
+            preg_match('/var downloads = \'(.*?)\'/', $lanzou, $down3);
+            if(empty($down3[1])){
+                preg_match('/var loaddown = \'(.*?)\'/', $lanzou, $down3);
+            } 
+            preg_match('/<div class=\\"md\\">(.*?)<span class=\\"mtt\\">\\((.*?)\\)<\\/span><\\/div>/', $lanzou, $size);
+            if (!empty($down2)){
+                $download = getRedirect($domain[1] . $down2[1]);
+            }else{
+                $download = getRedirect($domain[1] . $down3[1]);
+            }
+            if (strpos($lanzou,'请使用IOS自带浏览器safari下载')){
+                preg_match('/var nmousedows = \'(.*?)url=(.*?)\'/', $lanzou, $down);
+                preg_match('/var nmousedows = \'(.*?)\'\;/', $lanzou, $down1);
+                $arr = simplexml_load_string(curl($down[2]));
+                $down = json_decode(json_encode($arr),true);
+                $download = $down['dict']['array']['dict']['array']['dict'][0]['string'][1];
+                $install = $down1[1];
+            }
+            if (!empty($pwd)) {
+                preg_match('/sign\':\'(.*?)\'/', $lanzou, $sign);
+                $post_data = array('action' => 'downprocess', 'sign' => $sign[1], 'p' => $pwd);
+                $pwdurl = send_post('https://wwa.lanzoux.com/ajaxm.php', $post_data);
+                $obj = json_decode($pwdurl, true);
+                $download = getRedirect($obj['dom'] . '/file/' . $obj['url']);
+            }
+            $Json = array(
+                "code" => 200, 
+                "data" => array(
+                    "name" => $name[1], 
+                    "author" => $author[1], 
+                    "time" => $time[1], 
+                    "size" => $size[2], 
+                    "url" => $download
+                )
+            );
+            if(strpos($pwdurl,'"zt":0') !== false) {
+                $Json = array(
+                    "code" => 202, 
+                    "msg" => '密码错误',
+                );
+            }
         }
     }
-} else {
-    echo '请输入正确的蓝奏云分享的地址再试试！';
 }
+else{
+    $Json = array(
+        'code' => 201,
+        'msg' => '输入需要解析的蓝奏链接再试试吧'
+    );
+}
+if($type == 'down'){
+    header("Location:{$download}");
+}
+if (!empty($Json)) {
+    echo json_encode($Json, JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+}
+function send_post($url, $post_data)
+{
+    $postdata = http_build_query($post_data);
+    $options = array('http' => array(
+        'method' => 'POST',
+        'header' => 'Referer: https://www.lanzous.com/\\r\\n' . 'Accept-Language:zh-CN,zh;q=0.9\\r\\n',
+        'content' => $postdata,
+        'timeout' => 15 * 60,
+    ));
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    return $result;
+}
+
+function curl($url){
+	$header[] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+    $header[] = "Accept-Encoding: gzip, deflate, sdch, br";
+    $header[] = "Accept-Language: zh-CN,zh;q=0.8";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
+	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+	$output = curl_exec($ch);
+	curl_close($ch);
+	return $output;
+}
+
+function GetBetween($content, $start, $end)
+{
+    $r = explode($start, $content);
+    if (isset($r[1])) {
+        $r = explode($end, $r[1]);
+        return $r[0];
+    }
+    return '';
+}
+
+function getRedirect($url,$ref=''){
+        $headers = array(
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding: gzip, deflate',
+            'Accept-Language: zh-CN,zh;q=0.9',
+            'Cache-Control: no-cache',
+            'Connection: keep-alive',
+            'Pragma: no-cache',
+            'Upgrade-Insecure-Requests: 1',
+            'X-Forwarded-For: ' . rand_IP()
+        );
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER,$headers);
+        if ($ref) {
+            curl_setopt($curl, CURLOPT_REFERER, $ref);
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLINFO_HEADER_OUT, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $data = curl_exec($curl);
+        $url=curl_getinfo($curl);
+        curl_close($curl);
+        return $url["redirect_url"];
+    }
+    
+    function rand_IP(){
+        $ip2id = round(rand(600000, 2550000) / 10000);
+        $ip3id = round(rand(600000, 2550000) / 10000);
+        $ip4id = round(rand(600000, 2550000) / 10000);
+        $arr_1 = array("218","218","66","66","218","218","60","60","202","204","66","66","66","59","61","60","222","221","66","59","60","60","66","218","218","62","63","64","66","66","122","211");
+        $randarr= mt_rand(0,count($arr_1)-1);
+        $ip1id = $arr_1[$randarr];
+        return $ip1id.".".$ip2id.".".$ip3id.".".$ip4id;
+    }
